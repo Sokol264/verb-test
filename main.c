8df7716cc8708409866ce286f5b16f99c8957283
Danil Antonovich <sokoldaniil00@mail.ru>
2019-04-13 19:35:31 +0700

    Add Structure

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdlib.h>
 #include <string.h>
 
+struct List {
+    char I_form[20];
+    char II_form[20];
+    char III_form[20];
+    char Transl[20];
+};
+
 int main()
 {
     system("pause");
