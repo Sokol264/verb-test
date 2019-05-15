251991f0301d83a838517f349094f1eb6dfeb0fc
Nikilopik <palmovhero3@mail.ru>
2019-05-15 10:29:00 +0700

    Add mark output in file

--- src/rec.c
+++ src/rec.c
@@ -11,6 +11,19 @@ int recstat(char* login, int p, int w)
         printf("Error while creating file\n\n");
         return 1;
     }
+    float m=(p/30)*100;
+    if (m<=100 && m>90){
+        fprintf(rec, "5");
+    }
+    if (m<=90 && m>70){
+        fprintf(rec, "4");
+    }
+    if (m<=70 && m>50){
+        fprintf(rec, "3");
+    }
+    if (m<=50 && m>=0){
+        fprintf(rec, "2");
+    }
     fprintf(rec, "%d %d", p, w);
     fclose(rec);
     return 0;
