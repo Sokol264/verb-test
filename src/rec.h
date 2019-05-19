6eef67c940127c3b586c83bff084b0cb29999800
Max <Maks.latsigin@mail.ru>
2019-05-19 10:47:10 +0700

    Add mark calculation in recstat

--- src/rec.h
+++ src/rec.h
@@ -1,10 +1,11 @@
 #ifndef REC_H
 #define REC_H
 
+#include "mark.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-int recstat(char login[], int p, int w);
+int recstat(char login[], int p, int w, int k);
 
 #endif
